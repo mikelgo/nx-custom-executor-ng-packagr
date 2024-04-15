e30aaab8efa568a04f1651d5b858620d39c81a3f
Michael Berger <m.gonser@icloud.com>
2024-04-15 10:48:11 +0200

    init

---
+++ libs/components/btn/README.md
@@ -0,0 +1,3 @@
+# @styleguide-repro-nx/components/btn
+
+Secondary entry point of `@styleguide-repro-nx/components`. It can be used by importing from `@styleguide-repro-nx/components/btn`.
---
+++ libs/styleguide/src/lib/styleguide/styleguide.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-styleguide',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './styleguide.component.html',
+  styleUrl: './styleguide.component.css',
+})
+export class StyleguideComponent {}
