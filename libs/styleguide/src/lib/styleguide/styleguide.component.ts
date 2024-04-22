6481432199ee1673de662e262c00e7ded6baf27a
Michael Berger <m.gonser@icloud.com>
2024-04-22 09:20:13 +0200

    a

--- libs/styleguide/src/lib/styleguide/styleguide.component.ts
+++ libs/styleguide/src/lib/styleguide/styleguide.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ComponentsComponent } from '@styleguide-repro-nx/components';
+import { BtnModule } from '@styleguide-repro-nx/components/btn';
 
 @Component({
   selector: 'lib-styleguide',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ComponentsComponent, BtnModule],
   templateUrl: './styleguide.component.html',
   styleUrl: './styleguide.component.css',
 })
